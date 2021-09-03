e50f4d173275571b5286309d74a515e3cbd2d547
Josef Probst <josef.probst7@googlemail.com>
2021-09-03 12:31:46 +0200

    remove addpath

--- addpaths_turbo.m
+++ addpaths_turbo.m
@@ -1,7 +1,6 @@
 addpath(genpath('../bin/iso2mesh/'));
 addpath(genpath('../bin/mcx/mmc'));
 addpath(genpath('../bin/mcx/MCXStudio/MCXSuite/mmc'));
-addpath(genpath('../bin/mcx/mcxlab-linux-x86_64-nightlybuild/mcxlab/'));
 
 %addpath(genpath('../mcx/MCXStudio/MATLAB'));
 %addpath(genpath('../mcx/MCXStudio/MCXSuite/mcxcl/utils'));
